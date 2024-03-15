32a8ff5be58031c7320972354333ff52cc250b93
ChristianF22 <christianfonseca991@gmail.com>
2024-03-14 22:51:14 -0300

    tabelas do banco de dados criadas

---
+++ ProjetoES/src/main/java/br/com/projetoES/ProjetoES/ProjetoEsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoES.ProjetoES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEsApplication.class, args);
+	}
+
+}
---
+++ ProjetoES/src/test/java/br/com/projetoES/ProjetoES/ProjetoEsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoES.ProjetoES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
