b3177b562c4ea6df119443edf4fd0be121d26678
ChristianF22 <christianfonseca991@gmail.com>
2024-04-06 09:15:57 -0300

    implementação do arquivo database

---
+++ ProjetoES/database/CreateTable.sql
@@ -0,0 +1,22 @@
+create database projeto_integrado;
+use projeto_integrado;
+-- Tebela de usuarios
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(50) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    ativo TINYINT(1) DEFAULT 1,
+    data_nascimento DATE NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabela de Consultas
+CREATE TABLE consultas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    data_consulta DATE NOT NULL,
+    hora_consulta TIME,
+    descricao VARCHAR(500),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
---
+++ ProjetoES/database/InsertCadastro.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios (login, senha, nome, ativo, data_nascimento)
+VALUES ('Andre', '123', 'Andre Fontelene', 1, '2000-05-02');
+
+INSERT INTO usuarios (login, senha, nome, ativo, data_nascimento)
+VALUES ('Christian', 'cfsr', 'Christian fonseca', 1, '2003-6-24');
+
+INSERT INTO usuarios (login, senha, nome, ativo, data_nascimento)
+VALUES ('Leo', 'leo123', 'Leonardo Abreu', 1, '2003-05-12');
+
+INSERT INTO usuarios (login, senha, nome, ativo, data_nascimento)
+VALUES ('Gabriel', 'gh555', 'Gabriel Marques', 0, '2003-04-20');
