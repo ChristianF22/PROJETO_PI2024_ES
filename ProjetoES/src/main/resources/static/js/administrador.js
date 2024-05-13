ca3c1dfeaf5e33092a5120f9d3f7f04ddac5f690
Gabrielzzs <devgabriel.sousa@gmail.com>
2024-05-12 22:09:02 -0300

    Implementação da rota de administrador do site

---
+++ ProjetoES/src/main/resources/static/js/administrador.js
@@ -0,0 +1,12 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if (login === 'admin' && senha === 'admin1234') {
+        window.location.href = '/teste';
+    } else {
+        alert('Login ou senha incorretos. Por favor, tente novamente.');
+    }
+});
